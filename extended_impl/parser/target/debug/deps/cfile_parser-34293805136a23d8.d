2f466f413da5e46cb857f00361bdf343b31d491c
SiddheshKanawade <siddhesh.kanawade@iitgn.ac.in>
2024-08-03 12:57:34 +0530

    update the git ignore file

--- extended_impl/parser/target/debug/deps/cfile_parser-34293805136a23d8.d
+++
@@ -1,5 +0,0 @@
-/home/siddhesh/Desktop/Siddhesh/Nested Lock WCRT/nested_locks_new/extended_impl/parser/target/debug/deps/cfile_parser-34293805136a23d8: src/main.rs
-
-/home/siddhesh/Desktop/Siddhesh/Nested Lock WCRT/nested_locks_new/extended_impl/parser/target/debug/deps/cfile_parser-34293805136a23d8.d: src/main.rs
-
-src/main.rs:
