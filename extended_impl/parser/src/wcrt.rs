0f0bffc81087a6b3824f68fb656e3e840273dd07
SiddheshKanawade <siddhesh.kanawade@iitgn.ac.in>
2024-08-21 19:13:08 +0530

    wcrt implementation

--- extended_impl/parser/src/wcrt.rs
+++ extended_impl/parser/src/wcrt.rs
@@ -1,9 +1,44 @@
 use core::task;
-use std::cmp; // Import the `cmp` module for comparison functions
+use std::{cmp, f32}; // Import the `cmp` module for comparison functions
 
 // Calcuate the WCRT of the block
 // Input is all block list
 use crate::hardcode::Block;
+
+static THRESHOLD: f64 = 0.001;
+
+pub fn equalsf(x: f64, y: f64) -> bool {
+    return (x - y).abs() < THRESHOLD;
+}
+
+pub fn is_sat_eq2(init_li: f64, current_li: f64, high_tasks: Vec<&Block>) -> bool {
+    let mut new_li = init_li;
+
+    for task in high_tasks {
+        let time_period = task.time_period as f64;
+        let wcet = task.wcet;
+        new_li += (current_li / time_period).ceil() * wcet;
+    }
+
+    if equalsf(current_li, new_li) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+pub fn compute_l_ilk(current_li: f64, prev_li: f64, high_tasks: Vec<&Block>) -> f64 {
+    let mut new_li = current_li;
+
+    for task in high_tasks {
+        let time_period = task.time_period as f64;
+        let wcet = task.wcet;
+        new_li += (current_li / time_period).ceil() * wcet;
+        new_li -= (prev_li / time_period).ceil() * wcet;
+    }
+
+    return new_li;
+}
 
 pub fn loop_block(
     init_li: f64,
@@ -12,14 +47,13 @@ pub fn loop_block(
     block: &Block,
     all_block_list: Vec<Vec<&Block>>,
     task_number: usize,
-) {
+) -> f64 {
     // println!("{:?}", current_li);
     // println!("{:?}", prev_li);
-    println!("{:?}", block.block_type);
-    let mut prio = block.priority;
+    let prio = block.priority;
     let mut task_list: Vec<&Block> = Vec::new();
 
-    for task in all_block_list {
+    for task in all_block_list.iter() {
         task_list.push(task[0]);
     }
 
@@ -30,11 +64,32 @@ pub fn loop_block(
             high_tasks.push(task);
         }
     }
+
+    if !is_sat_eq2(init_li, current_li, high_tasks.clone()) && current_li < block.time_period as f64
+    {
+        // Calculate new li
+        let new_l_ilk = compute_l_ilk(current_li, prev_li, high_tasks);
+        return loop_block(
+            init_li,
+            new_l_ilk,
+            current_li,
+            block,
+            all_block_list,
+            task_number,
+        );
+    } else {
+        if current_li >= block.time_period as f64 {
+            println!("Block {:?} is not schedulable", block.block_type);
+            return -1.0;
+        } else {
+            return current_li;
+        }
+    }
 }
 
 pub fn calculate_wcrt_block(
     input_block: &Block,
-    all_block_list: Vec<Vec<&Block>>,
+    mut all_block_list: Vec<Vec<&Block>>,
     task_number: usize,
     block_number: usize,
 ) -> f64 {
@@ -97,11 +152,14 @@ pub fn calculate_wcrt_block(
             cij + waiting_time,
             0.0,
             input_block,
-            all_block_list,
+            all_block_list.clone(),
             task_number,
         );
 
-        return 0.0;
+        println!("{:?}", all_block_list[task_number][block_number].block_type);
+        println!("{:?}", wcrt);
+
+        return wcrt;
     }
 }
 pub fn calculate_wcrt(all_block_list: Vec<Vec<&Block>>) {
--- extended_impl/parser/target/debug/deps/cfile_parser-c9db4010edd0db10.d
+++
@@ -1,7 +0,0 @@
-/home/siddhesh/Desktop/Siddhesh/Nested Lock WCRT/nested_locks_new/extended_impl/parser/target/debug/deps/cfile_parser-c9db4010edd0db10: src/main.rs src/hardcode.rs src/wcrt.rs
-
-/home/siddhesh/Desktop/Siddhesh/Nested Lock WCRT/nested_locks_new/extended_impl/parser/target/debug/deps/cfile_parser-c9db4010edd0db10.d: src/main.rs src/hardcode.rs src/wcrt.rs
-
-src/main.rs:
-src/hardcode.rs:
-src/wcrt.rs:
